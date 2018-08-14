1195b378c1c675befa613588d7df6ddeec1e1d46
devapromix <bees@meta.ua>
2018-08-15 01:24:25 +0300

    Refactoring.

--- sources/uLanguage.pas
+++ sources/uLanguage.pas
@@ -95,8 +95,7 @@ begin
       S := SL[I];
       J := Pos('=', S);
       FID.Append(Copy(S, 1, J - 1));
-      Delete(S, 1, J);
-      FValue.Append(S);
+      FValue.Append(S.Remove(0, J));
     end;
   finally
     FreeAndNil(SL);
